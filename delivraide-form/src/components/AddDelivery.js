53be054821db623e7e309853e88d16f5c9cffdb6
Your Name <you@example.com>
2021-03-28 18:05:24 +0200

    added back end for delivery adding

--- delivraide-form/src/components/AddDelivery.js
+++ delivraide-form/src/components/AddDelivery.js
@@ -1,29 +1,48 @@
-import React from 'react'
+import React , { Component } from 'react'
+import { firestoreAutoId, createDelivery } from '../firebase';
 import './AddDelivery.css';
 
-function AddDelivery() {
+class AddDelivery extends Component {
+    state = {id: firestoreAutoId(), when: '', whath: '', from: 0, to: 0, to_adress: '', to_tel: 0, name: '', to_name: '', name2: '', tel: 0, adress: '', info_comp: '', taken: false, taken_by: "X", city:''};
+    handleChange = (e) => {
+        const { name, value } = e.target;
+
+        this.setState({ [name]: value });
+    };
+    handleSubmit = async (e) => {
+        e.preventDefault();
+        const {id, when, whath, from, to, to_adress, to_tel, name, to_name, name2, tel, adress, info_comp, taken, taken_by, city} = this.state;
+        try {
+            await createDelivery({ id }, { when }, { whath }, { from }, { to }, { to_adress }, { to_tel  } , { name } , { to_name }, { name2 }, { tel } , { adress } ,  { info_comp } , { taken } , { taken_by } , { city });
+        } catch (error) {
+            console.log('error', error);
+        }
+        this.setState({id: firestoreAutoId(), when: '', whath: '', from: 0, to: 0, to_adress: '', to_tel: 0, name: '', to_name: '', name2: '', tel: 0, adress: '', info_comp: '', taken: false, taken_by: "X", city:''});
+    };
+    render() {
+    const {when, whath, from, to, to_adress, to_tel, name, to_name, name2, tel, adress, info_comp, taken, taken_by, city} = this.state;
     return (
         <div className="main-wrapper">
             <div className="form-main-wrapper">
                 <p className="form-add-delivery-hero-title">Ajouter une livraison</p>
-                <form className="form-wrapper">
+                <form className="form-wrapper" onSubmit={this.handleSubmit}>
                     <div className="form-when-wrapper">
                         <div className="form-when">
                             <label>Quand ?</label>
-                            <input type="date" name="when" required></input>
+                            <input type="date" name="when" value={when} onChange={this.handleChange} required></input>
                         </div>
                         <div className="form-when-hour">
                             <label>A quelle heure ?</label>
-                            <input type="time" name="password" required></input>
+                            <input type="time" name="whath" value={whath} onChange={this.handleChange} required></input>
                         </div>
                         <div className="form-city-name">
                             <label>Ville ?</label>
-                            <select className="form-city-selector" name="city">
-                                <option value="Lyon">Lyon</option>
-                                <option value="Montpellier">Montreuil</option>
-                                <option value="Paris">Paris</option>
-                                <option value="Vélizy-Villacoublay">Vélizy-Villacoublay</option>
-                                <option value="Viroflay">Viroflay</option>
+                            <select className="form-city-selector" name="city" value={city} onChange={this.handleChange}>
+                                <option value="Lyon" onChange={this.handleChange}>Lyon</option>
+                                <option value="Montpellier" onChange={this.handleChange}>Montreuil</option>
+                                <option value="Paris" onChange={this.handleChange} >Paris</option>
+                                <option value="Vélizy-Villacoublay" onChange={this.handleChange}>Vélizy-Villacoublay</option>
+                                <option value="Viroflay" onChange={this.handleChange}>Viroflay</option>
                             </select>
                         </div>
                     </div>
@@ -31,21 +50,21 @@ function AddDelivery() {
                     <div className="form-city-info">
                         <div className="form-first-name">
                             <label>Enseigne :</label>
-                            <input type="name" name="name" required></input>
+                            <input type="name" name="to_name" value={to_name} onChange={this.handleChange} required></input>
                         </div>
                         <div className="form-number">
                             <label>Tél. :</label>
-                            <input type="tel" name="tel" required></input>
+                            <input type="tel" name="to_tel" value={to_tel} onChange={this.handleChange} required></input>
                         </div>
                     </div>
                     <div className="form-city-info">
                         <div className="form-city-name">
                             <label>Adresse :</label>
-                            <input type="text" name="adress" required></input>
+                            <input type="text" name="to_adress" value={to_adress} onChange={this.handleChange} required></input>
                         </div>
                         <div className="form-zip-code">
                             <label>Code postal :</label>
-                            <input type="number" name="code_postal" required></input>
+                            <input type="number" name="to" value={to} onChange={this.handleChange} required></input>
                         </div>
                     </div>
 
@@ -53,35 +72,35 @@ function AddDelivery() {
                     <div className="form-firstname-lastname">
                         <div className="form-last-name">
                             <label>Nom :</label>
-                            <input type="name" name="nom" required></input>
+                            <input type="name" name="name" value={name} onChange={this.handleChange} required></input>
                         </div>
                         <div className="form-first-name">
                             <label>Prénom :</label>
-                            <input type="name" name="prenom" required></input>
+                            <input type="name" name="name2" value={name2} onChange={this.handleChange} required></input>
                         </div>
                     </div>
                     <div className="form-city-info">
                         <div className="form-city-adress">
                             <label>Adresse :</label>
-                            <input type="text" name="adress" required></input>
+                            <input type="text" name="adress" value={adress} onChange={this.handleChange} required></input>
                         </div>
                         <div className="form-zip-code">
                             <label>Code postal :</label>
-                            <input type="number" name="code_postal" required></input>
+                            <input type="number" name="from" value={from} onChange={this.handleChange} required></input>
                         </div>
                     </div>
 
                     <div className="form-number">
                         <label>Tél. :</label>
-                        <input type="tel" name="tel" required></input>
+                        <input type="tel" name="tel" value={tel} onChange={this.handleChange} required></input>
                     </div>
                     <p className="form-add-delivery-hero-title">Informations complémentaires</p>
-                    <textarea maxLength="300" className="form-complementary-info" cols="50" swrap="hard" pellcheck="true"></textarea>
+                    <textarea maxLength="300" className="form-complementary-info" cols="50" swrap="hard" pellcheck="true" name="info_comp" value={info_comp} onChange={this.handleChange}></textarea>
                     <button type="submit" className="form-submit">Valider</button>
                 </form>
             </div>
         </div>
     )
 }
-
+}
 export default AddDelivery
--- delivraide-form/src/firebase.js
+++ delivraide-form/src/firebase.js
@@ -19,6 +19,70 @@ firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
+export const createDelivery = async (id1, when1, whath1, from1, to1, adress_to1, tel_to1, name1, to_name1, name3, tel1, adress1, info_comp1, taken1, taken_by1, city1) => {
+  
+  const userRef = firestore.doc(`av_deliveries/` + Object.values(id1));
+
+  const snapshot = await userRef.get();
+  console.log(id1)
+
+  if (!snapshot.exists) {
+    const { id } = id1;
+    console.log(id1)
+    const { when } = when1;
+    const { whath } = whath1;
+    const { from } = from1;
+    const { to } = to1;
+    const { to_adress } = adress_to1;
+    const { to_tel } = tel_to1;
+    const { name } = name1;
+    const { to_name } = to_name1;
+    const { name2 } = name3;
+    const { tel } = tel1;
+    const { adress } = adress1;
+    const { info_comp } = info_comp1;
+    const { taken } = taken1;
+    const { taken_by } = taken_by1;
+    const { city } = city1;
+
+    try {
+        await userRef.set({
+        id,
+        when,
+        whath,
+        from,
+        to,
+        to_adress,
+        to_tel,
+        name,
+        to_name,
+        name2,
+        tel,
+        adress,
+        info_comp,
+        taken,
+        taken_by,
+        city,
+      });
+    } catch (error) {
+      console.log('Error in adding delivery', error);
+    }
+  }
+};
+
+export const firestoreAutoId = () => {
+  const CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+  let autoId = ''
+
+  for (let i = 0; i < 20; i++) {
+    autoId += CHARS.charAt(
+      Math.floor(Math.random() * CHARS.length)
+    )
+  }
+  return autoId
+}
+
 export const createUserDocument = async (user, first_name, last_name, city, cp, nb_tel, bool) => {
   if (!user) return;
 
