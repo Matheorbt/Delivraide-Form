5b3ca08743cbe8d322695778a27511d00490fab2
MATHEO ROBERT <matho.robert@gmail.com>
2021-04-10 00:05:18 +0200

    Navbar done

--- delivraide-form/src/components/App.js
+++ delivraide-form/src/components/App.js
@@ -14,37 +14,56 @@ import AddDelivery from './AddDelivery'
 import Addnews from './Addnews'
 import logoAdmin from '../props/img/logo.png'
 import Dashboard from './Dashboard'
+import Application from './Application'
+import Home from './Home'
 
 function App() {
+    const displayNavbar = () => {
+        const navbarButton = document.querySelector(".navbar-button")
+        const navbarItems = document.querySelector(".admin-navbar-items")
+
+        navbarItems.classList.toggle('active-navbar');
+    }
     return (
         <Router>
             <nav className="admin-navbar">
                 <img src={logoAdmin} alt="logo admin"></img>
-                <ul className="admin-navbar-items">
+                <button className="navbar-button set-active" onClick={() => displayNavbar()}><i class="fa fa-bars" aria-hidden="true"></i></button>
+                <ul className="admin-navbar-items active-navbar">
+                    <button className="navbar-button set-unactive" onClick={() => displayNavbar()}><i class="fa fa-times" aria-hidden="true"></i></button>
                     <li>
-                        <Link to="/login">Login</Link>
+                        <Link to="/home">Accueil</Link>
                     </li>
                     <li>
-                        <Link to="/createuser">Create user</Link>
+                        <Link to="/login">Connexion</Link>
                     </li>
                     <li>
-                        <Link to="/userlist">User list</Link>
+                        <Link to="/createuser">Créer un utilisateur</Link>
                     </li>
                     <li>
-                        <Link to="/adduser">Add user</Link>
+                        <Link to="/userlist">Liste utilisateurs</Link>
                     </li>
                     <li>
-                        <Link to="/adddelivery">Add Delivery</Link>
+                        <Link to="/adduser">Ajouter un utilisateur</Link>
                     </li>
                     <li>
-                        <Link to="/addnews">Add News</Link>
+                        <Link to="/adddelivery">Ajouter une livraison</Link>
+                    </li>
+                    <li>
+                        <Link to="/addnews">Ajouter une news</Link>
                     </li>
                     <li>
                         <Link to="/dashboard">Dashboard</Link>
                     </li>
+                    <li>
+                        <Link to="/application">Candidature</Link>
+                    </li>
                 </ul>
             </nav>
             <Switch>
+                <Route path="/home">
+                    <Home />
+                </Route>
                 <Route path="/login">
                     <Login />
                 </Route>
@@ -65,6 +84,9 @@ function App() {
                 </Route>
                 <Route path="/dashboard">
                     <Dashboard />
+                </Route>
+                <Route path="/application">
+                    <Application />
                 </Route>
             </Switch>
         </Router>
---
+++ delivraide-form/src/components/Application.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Application.css'
+
+function Application() {
+    return (
+        <>
+            <h1>Application handling WIP</h1>
+        </>
+    )
+}
+
+export default Application
